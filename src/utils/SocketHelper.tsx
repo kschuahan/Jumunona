66fccb3bc49f12badd5349e007cf77eb2a875155
Kallu Singh Rajput <94896360+kschuahan@users.noreply.github.com>
2024-03-04 20:06:44 +0530

    Socket is done

--- src/utils/SocketHelper.tsx
+++ src/utils/SocketHelper.tsx
@@ -1,17 +1,24 @@
 import { Socket, io } from "socket.io-client";
 import { BASE_URL } from "../Netowork/Constants";
 import { localEnum } from "../Netowork/ApiEnum";
+import { userData } from "./AsyncStorage";
 
 let socket: Socket = null;
 
 const getSocket = async () => {
 
-    if (socket != null) {
-        socket =  await io(BASE_URL(localEnum.development))
-    }
+    socket = await io(BASE_URL(localEnum.development))
+    socket.on('connect', () => {
+        console.warn(socket.id) // true
 
-   
+    });
+    socket.emit("add-user", userData.userID)
+    socket.on("add-user", (userId: any) => {
+        console.warn("add-user", userId)
+    });
+
     return socket
+
 }
 export default getSocket;
 
